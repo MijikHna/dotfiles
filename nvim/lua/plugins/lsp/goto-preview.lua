6e4af96815409856dcbb26a35e5bd86fc0083457
MijikHna <safin88@gmx.de>
2025-03-16 22:53:36 +0100

    jsut a bit

--- nvim/lua/plugins/lsp/goto-preview.lua
+++ nvim/lua/plugins/lsp/goto-preview.lua
@@ -1,7 +1,17 @@
 return {
   "rmagatti/goto-preview",
   event = "BufEnter",
-  opts = {
-    default_mappings = true,
-  },
+  opts = {},
+  config = true,
+  init = function ()
+    local goto_preview = require("goto-preview")
+
+    vim.keymap.set("n", "<leader>pd", goto_preview.goto_preview_definition, {noremap=true, desc = "[P]review [D]efinitions"})
+    vim.keymap.set("n", "<leader>pi", goto_preview.goto_preview_implementation, {noremap=true, desc = "[P]review [I]mplementation"})
+    vim.keymap.set("n", "<leader>pD", goto_preview.goto_preview_declaration, {noremap=true, desc = "[P]review [D]eclaration"})
+    vim.keymap.set("n", "<leader>pr", goto_preview.goto_preview_references, {noremap=true, desc = "[P]review [R]eferences"})
+    vim.keymap.set("n", "<leader>pr", goto_preview.goto_preview_type_definition, {noremap=true, desc = "[P]review [T]ype Defintions"})
+
+    vim.keymap.set("n", "pc", goto_preview.close_all_win, {noremap=true, desc = "[P]review [C]lose All"})
+  end
 }
